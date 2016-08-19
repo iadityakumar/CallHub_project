0d9ea48b076a076538977038a4507542a74c9413
iadityakumar <kumar.aditya.02@gmail.com>
2016-08-19 18:45:41 +0530

    initial commit

---
+++ CallHub_project/utils.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+'''
+Matrix based fibonacci finding function
+'''
+from memoize import memoize, delete_memoized, delete_memoized_verhash
+
+fib_matrix = [[1,1],
+              [1,0]]
+
+def matrix_square(A, mod):
+    return mat_mult(A,A,mod)
+
+
+def mat_mult(A,B, mod):
+  if mod is not None:
+    return [[(A[0][0]*B[0][0] + A[0][1]*B[1][0])%mod, (A[0][0]*B[0][1] + A[0][1]*B[1][1])%mod],
+            [(A[1][0]*B[0][0] + A[1][1]*B[1][0])%mod, (A[1][0]*B[0][1] + A[1][1]*B[1][1])%mod]]
+
+
+def matrix_pow(M, power, mod):
+    #Special definition for power=0:
+    if power <= 0:
+      return M
+    powers =  list(reversed([True if i=="1" else False for i in bin(power)[2:]])) #Order is 1,2,4,8,16,...
+    matrices = [None for _ in powers]
+    matrices[0] = M
+    for i in range(1,len(powers)):
+        matrices[i] = matrix_square(matrices[i-1], mod)
+    result = None
+    for matrix, power in zip(matrices, powers):
+        if power:
+            if result is None:
+                result = matrix
+            else:
+                result = mat_mult(result, matrix, mod)
+    return result
+  
+
+@memoize(timeout=600000)
+def pow(x, n, I, mult):
+    """
+    Returns x to the power of n. Assumes I to be identity relative to the
+    multiplication given by mult, and n to be a positive integer.
+    """
+    if n == 0:
+        return I
+    elif n == 1:
+        return x
+    else:
+        
+        y = pow(x, n // 2, I, mult)
+            
+            
+        y = mult(y, y)
+        if n % 2:
+            y = mult(x, y)
+        return y
+
+
+def identity_matrix(n):
+    """Returns the n by n identity matrix."""
+    r = list(range(n))
+    return [[1 if i == j else 0 for i in r] for j in r]
+
+
+def matrix_multiply(A, B):
+    BT = list(zip(*B))
+    return [[sum(a * b
+                 for a, b in zip(row_a, col_b))
+            for col_b in BT]
+            for row_a in A]
+
+
+def fib(n):
+    F = pow([[1, 1], [1, 0]], n, identity_matrix(2), matrix_multiply)
+    return F[0][1]
+
+
